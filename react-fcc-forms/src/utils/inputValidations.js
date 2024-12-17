6ef962997619d9ea831b123df44e7410bea112df
Kelvin <kelvinmwendadoreen@gmail.com>
2024-12-17 13:57:10 +0300

    form validation with react-form hook...

---
+++ react-fcc-forms/src/utils/inputValidations.js
@@ -0,0 +1,106 @@
+/*-------------------------------------------------------------------
+|  🐼 Input Validators 
+|
+|  🐯 Purpose: THIS FILE CONTAINS ALL THE VALIDATORS OBJECTS
+|
+|  🐸 Returns:  -
+*-------------------------------------------------------------------*/
+
+export const name_validation = {
+  name: 'name',
+  label: 'name',
+  type: 'text',
+  id: 'name',
+  placeholder: 'write your name ...',
+  validation: {
+    required: {
+      value: true,
+      message: 'required',
+    },
+    maxLength: {
+      value: 30,
+      message: '30 characters max',
+    },
+    minLength:{
+      value:2,
+      message:"min 2 characters"
+    },
+  },
+}
+
+export const desc_validation = {
+  name: 'description',
+  label: 'description',
+  multiline: true,
+  id: 'description',
+  placeholder: 'write description ...',
+  validation: {
+    required: {
+      value: true,
+      message: 'required',
+    },
+    maxLength: {
+      value: 200,
+      message: '200 characters max',
+    },
+    minLength:{
+      value:4,
+      message:"min 4 characters"
+    },
+  },
+}
+
+export const password_validation = {
+  name: 'password',
+  label: 'password',
+  type: 'password',
+  id: 'password',
+  placeholder: 'Type password ...',
+  validation: {
+    required: {
+      value: true,
+      message: 'required',
+    },
+    minLength: {
+      value: 6,
+      message: 'min 6 characters',
+    },
+  },
+}
+
+export const num_validation = {
+  name: 'num',
+  label: 'number',
+  type: 'number',
+  id: 'num',
+  placeholder: 'write a random number',
+  validation: {
+    required: {
+      value: true,
+      message: 'required',
+    },
+    minLength: {
+      value: 10,
+      message:"Atleast 10 numbers",
+    }
+  },
+}
+
+export const email_validation = {
+  name: 'email',
+  label: 'email address',
+  type: 'email',
+  id: 'email',
+  placeholder: 'write a random email address',
+  validation: {
+    required: {
+      value: true,
+      message: 'required',
+    },
+    pattern: {
+      value:
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      message: 'Email is not valid',
+    },
+  },
+}
